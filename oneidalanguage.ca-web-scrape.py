1deee94f0f2ee63a905b7113f16f1cf719c72d3d
CutThroat <55604778+cutthroat78@users.noreply.github.com>
2022-03-30 11:14:29 +0100

    Update oneidalanguage.ca-web-scrape.py

--- oneidalanguage.ca-web-scrape.py
+++ oneidalanguage.ca-web-scrape.py
@@ -1,12 +1,6 @@
 # https://www.geeksforgeeks.org/python-web-scraping-tutorial/
 # https://stackoverflow.com/questions/59539194/how-to-download-all-mp3-url-as-mp3-from-a-webpage-using-python3
 
-# To-Do:
-# Get all words and phrases from https://oneidalanguage.ca/learn-our-language/oneidalanguage-words-phrases/?t_id=0 pages and put in csv file and put that file in seperate git repo
-# Ask for link and skill name
-# Put data (with skill name) in yaml file (and generate unique ID for skill and put into file)
-# Put audio files in desired folder structure (audio/$module_name/$skill_name/$oneida_word.mp3) and rename them to have the names of the Oneida words
-
 import requests
 from bs4 import BeautifulSoup
  
